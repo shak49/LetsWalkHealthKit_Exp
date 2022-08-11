7a542467fd338082a179c8983a271bcd5e4f0ad9
Shak49 <shakfeizi@gmali.com>
2022-08-11 14:37:55 -0500

    project's files added

---
+++ LetsWalkHealthKit_Exp/LetsWalkHealthKit_Exp/LetsWalkHealthKit_ExpApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsWalkHealthKit_ExpApp.swift
+//  LetsWalkHealthKit_Exp
+//
+//  Created by Shak Feizi on 8/11/22.
+//
+
+import SwiftUI
+
+@main
+struct LetsWalkHealthKit_ExpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
